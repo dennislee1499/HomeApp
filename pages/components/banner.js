ed22520542c8b8eb37883a092d2fe2854c6c08fc
Dennis Lee <dennislee1499@gmail.com>
2024-02-19 01:36:56 -0500

    style banner

--- pages/components/banner.js
+++ pages/components/banner.js
@@ -1,10 +1,12 @@
+import styles from "./banner.module.css";
+
 const Banner = () => {
     return (
-        <header>
-            <div>
-                <img src="./GloboLogo.png" alt="logo" />
+        <header className="row mb-4">
+            <div className="col-5">
+                <img src="./GloboLogo.png" alt="logo" className={styles.logo} />
             </div>
-            <div>
+            <div className={`${styles.subtitle} col-7 mt-5`}>
                 Providing houses all over the world
             </div>
         </header>
